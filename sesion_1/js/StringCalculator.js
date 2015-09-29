f08b3ae1ea4b148d3de3f3ee02da4a077b4d6b35
Juan Carlos Pérez Arriaga <elrevo@gmail.com>
2015-09-29 14:31:23 -0500

    Ejercicio 1
    Archivos de soporte para la Sesión 1 del DOJO

--- sesion_1/js/StringCalculator.js
+++ sesion_1/js/StringCalculator.js
@@ -1,3 +1,38 @@
 /*StringCalculator: 
 En este archivo se escribe el código JavasScript
 */
+/*TODO
+Continuar con pruebas avanzadas
+*/
+var Calculator={
+	add:function(numbers){	
+		var nums;
+		nums=Calculator.getNumbers(numbers);		
+		return numbers===""?0:Calculator.getSum(nums);
+	},
+	getSpecificDelimiter:function(numbers){
+		return numbers.substring(3,2);
+	},
+	getNumbers:function(numbers){
+		var nums;
+		var delimiter=/\n|,/;
+		if(numbers.indexOf("//")>-1){
+			delimiter=Calculator.getSpecificDelimiter(numbers);
+		}
+		nums=numbers.split(delimiter);
+		return nums;
+	},
+	getSum:function(nums){
+		var result=0;
+		for(var i=0;i<nums.length;i++){
+			if(!isNaN(parseInt(nums[i]))){
+				if(parseInt(nums[i])<0){
+					throw "Negativos no permitidos";
+				}else{
+					result=result+parseInt(nums[i]);
+				}
+			}
+		}
+		return result;
+	}
+};
--- sesion_1/js/tests.js
+++ sesion_1/js/tests.js
@@ -1,4 +1,13 @@
 /*En este archivo van los casos de prueba*/
 QUnit.test("Session 1: StringCalculator Tests", function(){
 	//TODO: escribir todos los casos de prueba
+	equal(Calculator.add(""),0,"Prueba cadena vacía");
+	equal(Calculator.add("5"),5,"Prueba suma de un número");
+	equal(Calculator.add("4,5"),9,"Prueba suma de 4+5=9");
+	equal(Calculator.add("1,2,3,4,5"),15,"Prueba suma de varios números");
+	equal(Calculator.add("1\n2,3"),6,"Prueba separador nueva línea");
+	equal(Calculator.add("//;\n1;2"),3,"Prueba separador específico");
+	throws(function(){
+		Calculator.add("1,2,-3")
+	},"Negativos no permitidos");
 });
